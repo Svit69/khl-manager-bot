a811169198b68812b3d82409f65cfa3601d49dbc
Svit69 <ol.svitenkov@gmail.com>
2026-02-09 00:29:20 +0500

    Adjust lineup to 3 full lines and 4th with 4 players

--- src/data/lineupBuilder.js
+++ src/data/lineupBuilder.js
@@ -1,8 +1,12 @@
 ﻿import { Line } from "../models/Line.js";
 import { PlayerPosition } from "../models/PlayerPosition.js";
 import { lineupScoreForPosition } from "../utils/positionFit.js";
-const LINE_WEIGHTS=[1,0.9,0.8,0.7];
-const LINE_POSITIONS=[PlayerPosition.LW,PlayerPosition.CTR,PlayerPosition.RW,PlayerPosition.DEF,PlayerPosition.DEF];
+const LINE_SCHEMAS=[
+  {weight:1,positions:[PlayerPosition.LW,PlayerPosition.CTR,PlayerPosition.RW,PlayerPosition.DEF,PlayerPosition.DEF]},
+  {weight:0.9,positions:[PlayerPosition.LW,PlayerPosition.CTR,PlayerPosition.RW,PlayerPosition.DEF,PlayerPosition.DEF]},
+  {weight:0.8,positions:[PlayerPosition.LW,PlayerPosition.CTR,PlayerPosition.RW,PlayerPosition.DEF,PlayerPosition.DEF]},
+  {weight:0.7,positions:[PlayerPosition.LW,PlayerPosition.CTR,PlayerPosition.RW,PlayerPosition.DEF]}
+];
 const takeBestPlayer=(players,slotPosition)=>{
   let bestIndex=-1,bestScore=-Infinity;
   players.forEach((player,index)=>{
@@ -13,14 +17,14 @@ const takeBestPlayer=(players,slotPosition)=>{
 };
 export const buildCompetitiveLines=roster=>{
   const pool=[...roster];
-  const lines=LINE_WEIGHTS.map(weight=>{
+  const lines=LINE_SCHEMAS.map(schema=>{
     const players=[];
-    LINE_POSITIONS.forEach(position=>{
+    schema.positions.forEach(position=>{
       if(!pool.length)return;
       const idx=takeBestPlayer(pool,position);
       players.push(pool.splice(idx,1)[0]);
     });
-    return new Line(players,weight,[...LINE_POSITIONS]);
+    return new Line(players,schema.weight,[...schema.positions]);
   });
   return {lines,reservePlayers:pool};
 };
